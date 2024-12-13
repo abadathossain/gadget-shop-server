3652ecd82434f624c99b9ed454b37e8e464e7152
Abadat Hossain <abadat26@gmail.com>
2024-12-14 00:15:54 +0600

    jwt and create operation done

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dxzduzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -19,11 +20,18 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+const userCollection = client.db("gadget-shop").collection("users");
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -39,6 +47,16 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Gadget shop is running");
 });
+
+app.post("/jwt", async (req, res) => {
+  const userEmail = req.body;
+  const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "3d",
+  });
+  res.send({ token });
+  console.log(userEmail);
+});
+
 app.listen(port, () => {
   console.log(`Gadget shop is running on port:${port}`);
 });
