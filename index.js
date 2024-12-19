f4328da4d56103fa94526582891d343d654a8676
Abadat Hossain <abadat26@gmail.com>
2024-12-19 23:22:48 +0600

    server ready

--- index.js
+++ index.js
@@ -14,6 +14,34 @@ app.use(
 );
 app.use(express.json());
 
+// token verification
+const verifyToken = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.send({ message: "No token" });
+  }
+  const token = authorization.split(" ")[1];
+  console.log(token);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.send({ message: "Invalid token" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+// verify seller
+const verifySeller = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  if (user?.role !== "seller") {
+    return res.send({ message: "forbideen access" });
+  }
+  next();
+};
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dxzduzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,6 +54,7 @@ const client = new MongoClient(uri, {
   },
 });
 const userCollection = client.db("gadget-shop").collection("users");
+const productCollection = client.db("gadget-shop").collection("products");
 
 async function run() {
   try {
@@ -35,7 +64,7 @@ async function run() {
     app.get("/user/:email", async (req, res) => {
       const query = { email: req.params.email };
       const result = await userCollection.findOne(query);
-      console.log(result);
+      console.log("Token:", result);
       res.send(result);
     });
 
@@ -45,6 +74,15 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
+    // create product
+
+    app.post("/add-products", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -65,10 +103,10 @@ app.get("/", (req, res) => {
 app.post("/jwt", async (req, res) => {
   const userEmail = req.body;
   const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "3d",
+    expiresIn: "10d",
   });
   res.send({ token });
-  // console.log(userEmail);
+  console.log("JWT email:", userEmail);
 });
 
 app.listen(port, () => {
